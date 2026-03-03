2424c039afd7426c3cd0b8507d4307cd9c851fd6
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-03 23:30:57 +0530

    This is what i learned on day 2

---
+++ Day-2.js
@@ -0,0 +1,35 @@
+/*
+let : can store
+1. Number : integer, float
+2. String : words
+3. Boolean : true / false
+4. Bigint
+5. Null
+6. Symbol
+7. Undefined
+
+*/
+
+let num = 13
+
+console.log (num)
+
+console.log (typeof num)  // tells the data type
+
+let str = "rover"
+
+console.log (str)
+
+console.log (typeof str)
+
+let bass = true
+
+console.log (bass)
+
+console.log (typeof bass)
+
+let name
+
+console.log(name)
+
+console.log (typeof name)
