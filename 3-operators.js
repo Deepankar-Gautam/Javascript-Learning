9d24e6c458583d8fb429f4b9a03e2c4b80a688d5
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-07 22:16:23 +0530

    Operators

---
+++ 3-operators.js
@@ -0,0 +1,80 @@
+/*===========Today's learnings===============
+
+==========Operators=============
+
+(1). Arithmatic Opeartors
+
+1. + Addition
+2. - Subtraction
+3. * Multiplication
+4. / Division
+5. % Remainder
+6. ** Power
+
+
+(2). Assignment Operators
+
+1. +=
+2. -=
+3. *=
+4. /=
+5. %=
+6. **=
+
+
+(3). Comparison Operators
+
+1. > Greater than
+2. < Less than
+3. >= Greater than or Equal to
+4. <= Less than or Equal to
+5. == Loose Equality (Avoid)          this compares the number with string, as the string is a numeric
+6. === Strict Equality (Preffer)      first check type, then comparison
+7. != Loose Inequality (Avoid)        this compares the number with string, as the string is a numeric
+8. !== Stric Inequality (Preffer)     first check type, then comparison 
+Note : in (5) if numeric string also have characters, then output will be "NaN"
+Note : NaN : not a number (number type)
+(4). Logical Operators
+
+1. && AND : Both must true
+2. || OR : One must true
+3. ! : Inverts the Boolean value
+
+*/
+
+// Arithmatic Opeartors
+
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/2)
+console.log(10%2)
+console.log(10**2)
+console.log()
+
+// Assignment Operators
+
+x = 20
+y = 10
+
+console.log (x+y)
+console.log (x-y)
+console.log (x*y)
+console.log (x/y)
+console.log (x%y)
+console.log (x**y)
+console.log()
+
+// Comparison Operators
+
+x = 20
+y = 10
+
+console.log(x>y)
+console.log(x<y)
+console.log(x>=y)
+console.log(x<=y)
+console.log(x===y)
+console.log(x!==y)
+console.log()
+
